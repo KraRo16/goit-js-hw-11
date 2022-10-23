938772cbdd173c0b2f1f1742f933de27292cc89c
Krasnozhon Roman <105301264+KraRo16@users.noreply.github.com>
2022-10-23 17:38:27 +0200

    first touch

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Notiflix from 'notiflix';
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
