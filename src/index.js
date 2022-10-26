e71131b65fd6a5eafe94d331ed79c074d1ab3e5d
Krasnozhon Roman <105301264+KraRo16@users.noreply.github.com>
2022-10-26 21:23:01 +0200

    upd css/ upd html/ add js API and variables

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 import Notiflix from 'notiflix';
-// Описан в документации
 import SimpleLightbox from 'simplelightbox';
-// Дополнительный импорт стилей
 import 'simplelightbox/dist/simple-lightbox.min.css';
+const axios = require('axios').default;
+
+const searchQuery = document.querySelector('input[name="searchQuery"]');
+const searchBtn = document.querySelector('.search-btn');
+const searchForm = document.querySelector('.search-form');
+const gallerySelector = document.querySelector('.gallery');
+
+//pixabayAPI
+
+const pixabayAPI = {
+  baseUrl: 'https://pixabay.com/api/',
+  key: '30805136-5107e54db1ae50bd847caa337',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: 'true',
+  order: 'popular',
+  page: '1',
+  per_page: '40',
+};
+
+//let variables
+
+let searchQueryResult = '';
+let q = '';
+let pageN = 1;
+let gallery = new SimpleLightbox('.gallery a', {
+  /* options */ enableKeyboard: true,
+});
